85b315775f8da595e1c108ff1ea1308adc629e69
devlinpadayachee@gmail.com <devlinpadayachee@gmail.com>
2017-08-12 22:28:45 +0200

    T2 Pic

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -61,7 +61,7 @@
 
                       <!--Card image-->
                       <div class="view">
-                          <img src="http://res.cloudinary.com/digitalgraft-co-za/image/upload/c_scale,e_grayscale,h_400,w_400/v1501201150/T_uxvoj3.jpg" class="img-fluid">
+                          <img src="http://res.cloudinary.com/digitalgraft-co-za/image/upload/c_scale,e_grayscale,h_400,w_400/v1502569567/T2.jpg" class="img-fluid">
                           <a href="#!">
                               <div class="mask"></div>
                           </a>
